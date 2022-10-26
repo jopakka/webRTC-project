2c56314f5c4413222855912685deb1af2ad43700
jopakka <jopakkamies@gmail.com>
2022-10-26 12:14:20 +0300

    createdAt uses server timestamp. AppSdpObserver to missing rtcclient fields

--- app/src/main/java/com/monitor/app/data/model/SensorInfo.kt
+++ app/src/main/java/com/monitor/app/data/model/SensorInfo.kt
@@ -1,8 +1,12 @@
 package com.monitor.app.data.model
 
+import com.google.firebase.firestore.ServerTimestamp
+import java.util.*
+
 data class SensorInfo(
     val name: String,
     val info: String,
-    val createdAt: Long = System.currentTimeMillis(),
+    @ServerTimestamp
+    val createdAt: Date? = null,
     val id: String? = null,
 )
