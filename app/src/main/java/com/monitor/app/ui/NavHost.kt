c166dc7bac3d2dd72637a7d0c6e8f70a735f8917
jopakka <jopakkamies@gmail.com>
2022-10-27 10:46:57 +0300

    Navigation screen names are enum. Save sensor id so connection can be recreated

--- app/src/main/java/com/monitor/app/ui/NavHost.kt
+++ app/src/main/java/com/monitor/app/ui/NavHost.kt
@@ -6,6 +6,7 @@ import androidx.navigation.NavHostController
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
+import com.monitor.app.core.Screens
 import com.monitor.app.ui.control.main.ControlMainScreen
 import com.monitor.app.ui.control.sensor.ControlSensorScreen
 import com.monitor.app.ui.sensor.init.SensorInitScreen
@@ -17,42 +18,46 @@ import com.monitor.app.ui.splashscreen.SplashScreen
 fun AppNavHost(
     modifier: Modifier = Modifier,
     navController: NavHostController = rememberNavController(),
-    startDestination: String = "splashScreen"
 ) {
     val user = "user-1"
     NavHost(
-        modifier = modifier, navController = navController, startDestination = startDestination
+        modifier = modifier, navController = navController, startDestination = Screens.SPLASH.name
     ) {
-        composable("splashScreen") {
-            SplashScreen { isMain ->
+        composable(Screens.SPLASH.name) {
+            SplashScreen { isMain, sensorId ->
                 navController.navigate(
-                    if (isMain) "controlMain"
-                    else "deviceTypeView"
+                    if (isMain)
+                        Screens.CONTROL_MAIN.name
+                    else if (sensorId != null) "${Screens.SENSOR_SEND.name}/$sensorId"
+                    else Screens.DEVICE_TYPE_VIEW.name
                 )
             }
         }
-        composable("deviceTypeView") {
+        composable(Screens.DEVICE_TYPE_VIEW.name) {
             DeviceTypeView { isMain ->
-                navController.navigate(if (isMain) "controlMain" else "sensorInit")
+                navController.navigate(
+                    if (isMain) Screens.CONTROL_MAIN.name
+                    else Screens.SENSOR_INIT.name
+                )
             }
         }
-        composable("sensorInit") {
+        composable(Screens.SENSOR_INIT.name) {
             SensorInitScreen(user) { id ->
-                navController.navigate("sensorSend/${id}")
+                navController.navigate("${Screens.SENSOR_SEND.name}/${id}")
             }
         }
-        composable("controlMain") {
+        composable(Screens.CONTROL_MAIN.name) {
             ControlMainScreen(user) { id ->
-                navController.navigate("sensorView/$id")
+                navController.navigate("${Screens.SENSOR_VIEW.name}/$id")
             }
         }
-        composable("sensorSend/{sensorID}") {
+        composable("${Screens.SENSOR_SEND.name}/{sensorID}") {
             val sensor = it.arguments?.getString("sensorID") ?: return@composable
             SensorMainScreen(user, sensor) {
                 navController.navigateUp()
             }
         }
-        composable("sensorView/{sensorID}") {
+        composable("${Screens.SENSOR_VIEW.name}/{sensorID}") {
             val sensor = it.arguments?.getString("sensorID") ?: return@composable
             ControlSensorScreen(user, sensor) {
                 navController.navigateUp()
--- app/src/main/java/com/monitor/app/ui/splashscreen/SplashScreen.kt
+++ app/src/main/java/com/monitor/app/ui/splashscreen/SplashScreen.kt
@@ -1,6 +1,5 @@
 package com.monitor.app.ui.splashscreen
 
-import android.util.Log
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
@@ -8,30 +7,17 @@ import androidx.compose.material.CircularProgressIndicator
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.LaunchedEffect
-import androidx.compose.runtime.collectAsState
-import androidx.compose.runtime.getValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.style.TextAlign
 import com.monitor.app.R
-import com.monitor.app.data.utils.DataStoreUtil
+import com.monitor.app.core.components.GetDataStoreValues
 
 @Composable
-fun SplashScreen(onDeviceFound: (isMain: Boolean) -> Unit) {
-    val context = LocalContext.current
-    val dataStore = DataStoreUtil(context)
-    val savedDeviceTypeIsMain by dataStore.getDeviceType.collectAsState(initial = null)
-
-    /**
-     * Inside let, navigate to corresponding view
-     * based on value of savedDeviceType
-     */
-    LaunchedEffect(key1 = savedDeviceTypeIsMain) {
-        Log.d("SplashScreen", "savedDeviceTypeIsMain=$savedDeviceTypeIsMain")
-        onDeviceFound(savedDeviceTypeIsMain == true)
+fun SplashScreen(onDataStore: (isMain: Boolean, sensorId: String?) -> Unit) {
+    GetDataStoreValues { isMain, id ->
+        onDataStore(isMain, id)
     }
 
     Column(
