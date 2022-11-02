4b514022a93f26be6d768fdc452e06d43ddfe66a
Patrik Pölkki <patrik.polkki@elisa.fi>
2022-11-02 12:43:01 +0200

    Previewlistitem works. Also added little bit of elevation to listitem

--- app/src/main/java/com/monitor/app/core/components/SensorListItem.kt
+++ app/src/main/java/com/monitor/app/core/components/SensorListItem.kt
@@ -9,6 +9,7 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
+import androidx.compose.ui.draw.shadow
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.Shape
 import androidx.compose.ui.text.TextStyle
@@ -22,7 +23,8 @@ import com.monitor.app.data.model.SensorInfo
 @OptIn(ExperimentalMaterialApi::class)
 @Composable
 fun SensorListItem(
-    sensorInfo: SensorInfo, onClick: (id: String) -> Unit,
+    sensorInfo: SensorInfo,
+    onClick: (id: String) -> Unit,
     modifier: Modifier = Modifier,
     titleColor: Color = Color.White,
     titleSize: TextUnit = MaterialTheme.typography.h6.fontSize,
@@ -31,54 +33,60 @@ fun SensorListItem(
     borderShape: Shape = RoundedCornerShape(size = 10.dp),
 ) {
 
-    Column(
-        modifier = modifier
-            .clip(borderShape)
-            .background(Color(0xFFF3F3F3))
-            .clickable {
-                onClick(sensorInfo.id ?: "")
-            },
+    Surface(
+        modifier = modifier.shadow(3.dp, shape = borderShape)
     ) {
-        Row(
+        Column(
             modifier = modifier
-                .background(Color(0xFFE39E37))
-                .padding(horizontal = 18.dp, vertical = 14.dp),
-            verticalAlignment = Alignment.CenterVertically
+                .clip(borderShape)
+                .background(Color(0xFFF3F3F3))
+                .clickable {
+                    onClick(sensorInfo.id ?: "")
+                },
         ) {
-            Text(
+            Row(
                 modifier = modifier
-                    .weight(8f),
-                text = sensorInfo.name.uppercase(),
-                style = TextStyle(
-                    color = titleColor, fontSize = titleSize, fontWeight = titleWeight
-                ),
-                maxLines = 1,
-                overflow = TextOverflow.Ellipsis
-            )
-            BatteryLevel(batteryLevel = sensorInfo.battery)
-        }
-        Column(modifier = modifier.padding(all = 18.dp)) {
-            Text(
-                text = "Description:",
-                style = TextStyle(
-                    color = subtitleColor, fontSize = MaterialTheme.typography.body2.fontSize
+                    .background(Color(0xFFE39E37))
+                    .padding(horizontal = 18.dp, vertical = 14.dp),
+                verticalAlignment = Alignment.CenterVertically
+            ) {
+                Text(
+                    modifier = modifier
+                        .weight(8f),
+                    text = sensorInfo.name.uppercase(),
+                    style = TextStyle(
+                        color = titleColor, fontSize = titleSize, fontWeight = titleWeight
+                    ),
+                    maxLines = 1,
+                    overflow = TextOverflow.Ellipsis
                 )
-            )
-            Text(
-                text = sensorInfo.info,
-                style = TextStyle(
-                    color = subtitleColor, fontSize = MaterialTheme.typography.body1.fontSize
-                ),
-                maxLines = 3,
-                overflow = TextOverflow.Ellipsis
-            )
-        }
+                BatteryLevel(batteryLevel = sensorInfo.battery)
+            }
+            Column(modifier = modifier.padding(all = 18.dp)) {
+                Text(
+                    text = "Description:",
+                    style = TextStyle(
+                        color = subtitleColor, fontSize = MaterialTheme.typography.body2.fontSize
+                    )
+                )
+                Text(
+                    text = sensorInfo.info,
+                    style = TextStyle(
+                        color = subtitleColor, fontSize = MaterialTheme.typography.body1.fontSize
+                    ),
+                    maxLines = 3,
+                    overflow = TextOverflow.Ellipsis
+                )
+            }
 
+        }
     }
 }
 
 @Preview
 @Composable
 fun PreviewListItem() {
-    // SensorListItem(SensorInfo("Testi", "Jee")) {}
+    SensorListItem(
+        SensorInfo("Living Room", "Old galaxy phone positioned to next to firebase"),
+        onClick = {})
 }
