6a2593e349c4cf9bd36eddaa4a567fdb8e736739
Patrik Pölkki <patrik.polkki@elisa.fi>
2022-11-02 21:15:28 +0200

    SelectableItem.kt colors changed. DeviceTypeView UI changed to look little bit better

--- app/src/main/java/com/monitor/app/ui/shared/DeviceTypeView.kt
+++ app/src/main/java/com/monitor/app/ui/shared/DeviceTypeView.kt
@@ -1,15 +1,15 @@
 package com.monitor.app.ui.shared
 
 import androidx.compose.foundation.layout.*
-import androidx.compose.material.Button
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Text
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.*
 import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.stringResource
-import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.unit.dp
 import com.monitor.app.R
 import com.monitor.app.core.components.SelectableItem
@@ -24,42 +24,69 @@ fun DeviceTypeView(onDeviceSelected: (isMain: Boolean) -> Unit) {
 
     var deviceTypeIsMain: Boolean? by remember { mutableStateOf(null) }
 
-    Column(
-        modifier = Modifier.fillMaxSize(),
-        verticalArrangement = Arrangement.SpaceAround,
-        horizontalAlignment = Alignment.CenterHorizontally
-    ) {
-        Text(
-            text = stringResource(R.string.choose_device_type),
-            style = MaterialTheme.typography.h4,
-            textAlign = TextAlign.Center
-        )
-        Column(modifier = Modifier.padding(horizontal = 60.dp)) {
-            SelectableItem(
-                selected = deviceTypeIsMain ?: false,
-                title = stringResource(R.string.device_main),
-                subtitle = stringResource(R.string.device_main_info)
+    Scaffold(
+        topBar = {
+            TopAppBar(
+                title = { Text(text = "House Monitor System") },
+                backgroundColor = Color(0xFFE39E37),
+                contentColor = Color.White,
+                elevation = 10.dp
+            )
+        },
+        content = { paddingValues ->
+            Column(
+                modifier = Modifier
+                    .fillMaxSize()
+                    .padding(paddingValues),
+                verticalArrangement = Arrangement.Center,
+                horizontalAlignment = Alignment.CenterHorizontally
             ) {
-                deviceTypeIsMain = it
-            }
-            Spacer(modifier = Modifier.height(12.dp))
-            SelectableItem(
-                selected = deviceTypeIsMain?.let { !it } ?: false,
-                title = stringResource(R.string.device_sensor),
-                subtitle = stringResource(R.string.device_sensor_info)
-            ) {
-                deviceTypeIsMain = !it
-            }
-        }
+                Text(
+                    text = stringResource(R.string.choose_device_type),
+                    style = MaterialTheme.typography.h4,
+                    maxLines = 1,
+                    overflow = TextOverflow.Ellipsis
+                )
+                Column(modifier = Modifier.padding(horizontal = 60.dp, vertical = 40.dp)) {
+                    SelectableItem(
+                        selected = deviceTypeIsMain ?: false,
+                        title = stringResource(R.string.device_main),
+                        subtitle = stringResource(R.string.device_main_info)
+                    ) {
+                        deviceTypeIsMain = it
+                    }
+                    Spacer(modifier = Modifier.height(12.dp))
+                    SelectableItem(
+                        selected = deviceTypeIsMain?.let { !it } ?: false,
+                        title = stringResource(R.string.device_sensor),
+                        subtitle = stringResource(R.string.device_sensor_info)
+                    ) {
+                        deviceTypeIsMain = !it
+                    }
+                }
 
-        Button(onClick = {
-            scope.launch {
-                val isMain = deviceTypeIsMain ?: return@launch
-                dataStore.saveDeviceType(isMain)
-                onDeviceSelected(isMain)
+                Button(onClick = {
+                    scope.launch {
+                        val isMain = deviceTypeIsMain ?: return@launch
+                        dataStore.saveDeviceType(isMain)
+                        onDeviceSelected(isMain)
+                    }
+                },
+                    enabled = deviceTypeIsMain?.let { true } ?: false,
+                    colors = ButtonDefaults.buttonColors(backgroundColor = Color(0xFFE39E37)),
+                    shape = RoundedCornerShape(50.dp),
+                    modifier = Modifier
+                        .padding(horizontal = 60.dp)
+                        .fillMaxWidth()
+                )
+                {
+                    Text(
+                        text = stringResource(R.string.next),
+                        color = Color.White,
+                        modifier = Modifier.padding(8.dp)
+                    )
+                }
             }
-        }, enabled = deviceTypeIsMain?.let { true } ?: false) {
-            Text(text = stringResource(R.string.next))
-        }
-    }
+        })
+
 }
