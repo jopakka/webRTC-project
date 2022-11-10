5db8382ef99030886886d4491c7544fc33c604fb
Patrik Pölkki <patrik.polkki@elisa.fi>
2022-11-10 14:09:40 +0200

    Show corresponding batterylevel icon

--- app/src/main/java/com/monitor/app/core/components/BatteryLevel.kt
+++ app/src/main/java/com/monitor/app/core/components/BatteryLevel.kt
@@ -14,12 +14,36 @@ import com.monitor.app.R
 
 @Composable
 fun BatteryLevel(batteryLevel: Int?) {
-
-
+    val batteryLevelIcon = when (batteryLevel) {
+        100 -> {
+            R.drawable.ic_baseline_battery_full_24
+        }
+        in 85..99 -> {
+            R.drawable.ic_baseline_battery_6_bar_24
+        }
+        in 68..84 -> {
+            R.drawable.ic_baseline_battery_5_bar_24
+        }
+        in 52..68 -> {
+            R.drawable.ic_baseline_battery_4_bar_24
+        }
+        in 34..51 -> {
+            R.drawable.ic_baseline_battery_3_bar_24
+        }
+        in 17..33 -> {
+            R.drawable.ic_baseline_battery_2_bar_24
+        }
+        in 1..16 -> {
+            R.drawable.ic_baseline_battery_1_bar_24
+        }
+        else -> {
+            R.drawable.ic_baseline_battery_0_bar_24
+        }
+    }
     Row(verticalAlignment = Alignment.Bottom) {
         Icon(
-            painter = painterResource(id = R.drawable.ic_baseline_battery_4_bar_24),
-            contentDescription = "moro",
+            painter = painterResource(id = batteryLevelIcon),
+            contentDescription = "Battery level",
             tint = Color.White
         )
         Text(
