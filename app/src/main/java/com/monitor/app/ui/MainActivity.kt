7f2ce85122e399da8eef3af9961d54ff722bcefc
Mirohaut <74530090+Mirohaut@users.noreply.github.com>
2022-11-11 15:01:21 +0200

    auth (#29)
    * auth
    
    * asd

--- app/src/main/java/com/monitor/app/ui/MainActivity.kt
+++ app/src/main/java/com/monitor/app/ui/MainActivity.kt
@@ -1,19 +1,30 @@
 package com.monitor.app.ui
-
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.navigation.compose.NavHost
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.ktx.Firebase
 import com.monitor.app.core.theme.HomeSecuritySystemTheme
+import com.monitor.app.ui.autentication.LoginScreen
 import kotlinx.coroutines.ExperimentalCoroutinesApi
 
 @ExperimentalCoroutinesApi
 class MainActivity : ComponentActivity() {
+
+    private val auth by lazy {
+        Firebase.auth
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
             HomeSecuritySystemTheme(false) {
+                //*LoginScreen(auth)
                 AppNavHost(this)
+
             }
         }
     }
 }
+
---
+++ app/src/main/java/com/monitor/app/ui/autentication/Login.kt
@@ -0,0 +1,212 @@
+package com.monitor.app.ui.autentication
+
+import android.util.Log
+import android.util.Patterns
+import androidx.compose.foundation.BorderStroke
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.foundation.text.KeyboardActions
+import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.material.*
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Clear
+import androidx.compose.material.icons.filled.Visibility
+import androidx.compose.material.icons.filled.VisibilityOff
+import androidx.compose.runtime.*
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.focus.FocusDirection
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.LocalFocusManager
+import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.FontStyle
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.input.ImeAction
+import androidx.compose.ui.text.input.KeyboardType
+import androidx.compose.ui.text.input.PasswordVisualTransformation
+import androidx.compose.ui.text.input.VisualTransformation
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.ktx.Firebase
+import com.monitor.app.core.theme.HomeSecuritySystemTheme
+
+
+@Composable
+fun LoginScreen(auth: FirebaseAuth) {
+
+    val focusManager = LocalFocusManager.current
+
+    var email by remember {
+        mutableStateOf("")
+    }
+
+
+    var password by remember {
+        mutableStateOf("")
+    }
+
+    val isEmailValid by derivedStateOf {
+        Patterns.EMAIL_ADDRESS.matcher(email).matches()
+    }
+
+    val isPasswordValid by derivedStateOf {
+        password.length > 4
+    }
+
+    var isPasswordVisible by remember {
+        mutableStateOf(false)
+    }
+
+    Column(
+        modifier = Modifier
+            .background(color = Color.LightGray)
+            .fillMaxSize(),
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.Top
+    ) {
+        Text(
+            text = "Hi",
+            fontFamily = FontFamily.SansSerif,
+            fontWeight = FontWeight.Bold,
+            fontStyle = FontStyle.Italic,
+            fontSize = 32.sp,
+            modifier = Modifier.padding(top = 16.dp)
+
+
+        )
+
+
+
+        Text(
+            text = "House Monitoring System",
+            fontFamily = FontFamily.SansSerif,
+            fontWeight = FontWeight.Bold,
+            fontStyle = FontStyle.Italic,
+            fontSize = 32.sp,
+            modifier = Modifier.padding(bottom = 16.dp)
+
+
+        )
+
+        Card(
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(horizontal = 8.dp),
+            shape = RoundedCornerShape(16.dp),
+            border = BorderStroke(1.dp, Color.Black)
+        ){
+            Column(
+                horizontalAlignment = Alignment.CenterHorizontally,
+                verticalArrangement = Arrangement.spacedBy(8.dp),
+                modifier = Modifier.padding(all = 10.dp)
+            ) {
+                OutlinedTextField(value = email,
+                    onValueChange = { email = it },
+
+                    label = { Text(text = "Email Address") },
+                    placeholder = { Text(text = "abc@gmail.com")},
+                    singleLine = true,
+                    modifier = Modifier.fillMaxWidth(),
+                    keyboardOptions = KeyboardOptions(
+                        keyboardType = KeyboardType.Email,
+                        imeAction = ImeAction.Next
+                    ),
+                    keyboardActions = KeyboardActions(
+                        onNext = {focusManager.moveFocus(FocusDirection.Down)}
+                    ),
+                    isError = !isEmailValid,
+                    trailingIcon = {
+                        if (email.isNotBlank()) {
+                            IconButton(onClick = { email = ""}) {
+                                Icon(
+                                    imageVector = Icons.Filled.Clear,
+                                    contentDescription = "Clear email"
+                                )
+
+                            }
+                        }
+                    },
+                )
+
+                OutlinedTextField(value = password,
+                    onValueChange = { password = it },
+
+                    label = { Text(text = "Password") },
+                    singleLine = true,
+                    modifier = Modifier.fillMaxWidth(),
+                    keyboardOptions = KeyboardOptions(
+                        keyboardType = KeyboardType.Password,
+                        imeAction = ImeAction.Done
+                    ),
+                    keyboardActions = KeyboardActions(
+                        onDone = {focusManager.clearFocus()}
+                    ),
+                    isError = !isPasswordValid,
+                    trailingIcon = {
+                        IconButton(onClick = { isPasswordVisible = !isPasswordVisible}){
+                            Icon(imageVector = if (isPasswordVisible) Icons.Default.Visibility else Icons.Default.VisibilityOff,
+                                contentDescription = "Toggle Password visibility")
+                        }
+                    },
+                    visualTransformation = if(isPasswordVisible) VisualTransformation.None else PasswordVisualTransformation()
+                )
+
+                Button(onClick = {
+                                 auth.signInWithEmailAndPassword(email, password)
+                                     .addOnCompleteListener{
+                                         Log.d("asd", "testi")
+                                     }
+                },
+                    modifier = Modifier.fillMaxWidth(),
+                    colors = ButtonDefaults.buttonColors(backgroundColor = Color.Red),
+                    enabled = isEmailValid && isPasswordValid) {
+                    Text(text = "Log in",
+                        fontWeight = FontWeight.Bold,
+                        color = Color.Black,
+                        fontSize = 16.sp)
+                }
+            }
+        }
+        Row(
+            horizontalArrangement = Arrangement.End,
+            modifier = Modifier.fillMaxWidth()
+        ){
+            TextButton(onClick = {}) {
+                Text(
+                    color = Color.Black,
+                    fontStyle = FontStyle.Italic,
+                    text = "Forgot password?",
+                    modifier = Modifier.padding(end = 8.dp)
+                )
+            }
+        }
+
+        Button(
+            onClick = {},
+            enabled = true,
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(all = 16.dp),
+            colors = ButtonDefaults.buttonColors(backgroundColor = Color.White)
+        ){
+            Text(
+                text = "Register",
+                fontWeight = FontWeight.Bold,
+                color = Color.Black,
+                fontSize = 16.sp
+            )
+        }
+    }
+
+}
+
+
+@Composable
+fun DefaultPreview(){
+    HomeSecuritySystemTheme {
+        LoginScreen(Firebase.auth)
+    }
+}
