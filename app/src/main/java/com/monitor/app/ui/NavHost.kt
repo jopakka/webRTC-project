bdc7c023be9a5a8d48ea1a20fc91d3bbf8fe5de9
jopakka <jopakkamies@gmail.com>
2022-11-16 09:45:00 +0200

    new logo and added login to nav logic

--- app/src/main/java/com/monitor/app/ui/NavHost.kt
+++ app/src/main/java/com/monitor/app/ui/NavHost.kt
@@ -1,14 +1,16 @@
 package com.monitor.app.ui
 
 import android.app.Activity
-import androidx.compose.runtime.Composable
+import androidx.compose.runtime.*
 import androidx.compose.ui.Modifier
 import androidx.navigation.NavHostController
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
+import com.google.firebase.auth.FirebaseAuth
 import com.monitor.app.core.DeviceTypes
 import com.monitor.app.core.Screens
+import com.monitor.app.ui.autentication.LoginScreen
 import com.monitor.app.ui.control.main.ControlMainScreen
 import com.monitor.app.ui.control.sensor.ControlSensorScreen
 import com.monitor.app.ui.sensor.init.SensorInitScreen
@@ -19,29 +21,44 @@ import com.monitor.app.ui.splashscreen.SplashScreen
 @Composable
 fun AppNavHost(
     activity: Activity,
+    auth: FirebaseAuth,
     modifier: Modifier = Modifier,
     navController: NavHostController = rememberNavController(),
 ) {
-    val TAG = "AppNavHost"
-    val user = "user-1"
+    var userId by remember {
+        mutableStateOf(auth.currentUser?.uid)
+    }
     NavHost(
         modifier = modifier, navController = navController, startDestination = Screens.SPLASH.name
     ) {
         composable(Screens.SPLASH.name) {
             SplashScreen { deviceType, sensorId ->
-                val navDir = when (deviceType) {
-                    DeviceTypes.NONE -> Screens.DEVICE_TYPE_VIEW.name
-                    DeviceTypes.MAIN -> Screens.CONTROL_MAIN.name
-                    DeviceTypes.SENSOR -> {
+                val navDir = when {
+                    auth.currentUser == null -> Screens.AUTH.name
+                    deviceType == DeviceTypes.NONE -> Screens.DEVICE_TYPE_VIEW.name
+                    deviceType == DeviceTypes.MAIN -> Screens.CONTROL_MAIN.name
+                    deviceType == DeviceTypes.SENSOR -> {
                         if (sensorId.isNotBlank()) {
                             "${Screens.SENSOR_SEND.name}/$sensorId"
                         } else {
                             Screens.DEVICE_TYPE_VIEW.name
                         }
                     }
+                    else -> return@SplashScreen
                 }
                 navController.navigate(navDir) {
                     popUpTo(Screens.SPLASH.name) {
+                        inclusive = true
+                    }
+                    launchSingleTop = true
+                }
+            }
+        }
+        composable(Screens.AUTH.name) {
+            LoginScreen(auth) { id ->
+                userId = id
+                navController.navigate(Screens.SPLASH.name) {
+                    popUpTo(Screens.AUTH.name) {
                         inclusive = true
                     }
                     launchSingleTop = true
@@ -60,24 +77,24 @@ fun AppNavHost(
             }
         }
         composable(Screens.SENSOR_INIT.name) {
-            SensorInitScreen(user) { id ->
+            SensorInitScreen(userId!!) { id ->
                 navController.navigate("${Screens.SENSOR_SEND.name}/${id}")
             }
         }
         composable(Screens.CONTROL_MAIN.name) {
-            ControlMainScreen(user) { id ->
+            ControlMainScreen(userId!!) { id ->
                 navController.navigate("${Screens.SENSOR_VIEW.name}/$id")
             }
         }
         composable("${Screens.SENSOR_SEND.name}/{sensorID}") {
             val sensor = it.arguments?.getString("sensorID") ?: return@composable
-            SensorMainScreen(user, sensor) {
+            SensorMainScreen(userId!!, sensor) {
                 activity.finish()
             }
         }
         composable("${Screens.SENSOR_VIEW.name}/{sensorID}") {
             val sensor = it.arguments?.getString("sensorID") ?: return@composable
-            ControlSensorScreen(user, sensor) {
+            ControlSensorScreen(userId!!, sensor) {
                 navController.navigateUp()
             }
         }
