1f4aef3d2cc2bee691df832f92552f4a74c75bbf
jopakka <jopakkamies@gmail.com>
2022-11-24 14:00:41 +0200

    sensorlist background

--- app/src/main/java/com/monitor/app/core/components/SensorList.kt
+++ app/src/main/java/com/monitor/app/core/components/SensorList.kt
@@ -2,10 +2,8 @@ package com.monitor.app.core.components
 
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
-import androidx.compose.material.Surface
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
@@ -15,14 +13,13 @@ import com.monitor.app.data.rtcclient.model.SensorInfo
 
 @Composable
 fun SensorList(sensors: List<SensorInfo>, itemOnClick: (id: String) -> Unit) {
-    Surface(modifier = Modifier.fillMaxSize()) {
-        LazyColumn(
-            modifier = Modifier.padding(horizontal = 18.dp, vertical = 12.dp),
-            verticalArrangement = Arrangement.spacedBy(10.dp)
-        ) {
-            items(sensors) { sensor ->
-                SensorListItem(sensor, itemOnClick)
-            }
+    LazyColumn(
+        modifier = Modifier
+            .fillMaxSize(),
+        verticalArrangement = Arrangement.spacedBy(10.dp)
+    ) {
+        items(sensors) { sensor ->
+            SensorListItem(sensor, itemOnClick)
         }
     }
 }
