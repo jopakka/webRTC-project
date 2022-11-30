4b3d51af7e9ecca3b5f70a04d3c1d23a1ce4547b
jopakka <jopakkamies@gmail.com>
2022-11-30 13:14:57 +0200

    made SplashScreen better looking

--- app/src/main/java/com/monitor/app/core/theme/Color.kt
+++ app/src/main/java/com/monitor/app/core/theme/Color.kt
@@ -2,7 +2,7 @@ package com.monitor.app.core.theme
 
 import androidx.compose.ui.graphics.Color
 
-val GreenBackground = Color(0xFF94D88C)
+val GreenBackground = Color(0xFFE8E8E8)
 val Green200 = Color(0xFF11C765)
 val Green500 = Color(0xFF0E8E49)
 val Green700 = Color(0xFF0B5F31)
--- app/src/main/java/com/monitor/app/ui/splashscreen/SplashScreen.kt
+++ app/src/main/java/com/monitor/app/ui/splashscreen/SplashScreen.kt
@@ -1,10 +1,7 @@
 package com.monitor.app.ui.splashscreen
 
 import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.*
 import androidx.compose.material.CircularProgressIndicator
 import androidx.compose.material.Icon
 import androidx.compose.material.MaterialTheme
@@ -12,10 +9,14 @@ import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.colorResource
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.monitor.app.R
 import com.monitor.app.core.DeviceTypes
@@ -35,15 +36,30 @@ fun SplashScreen(onDataStore: (deviceType: DeviceTypes, sensorId: String) -> Uni
         horizontalAlignment = Alignment.CenterHorizontally
     ) {
         Icon(
-            painter = painterResource(R.drawable.ic_launcher_new_foreground),
+            painter = painterResource(R.drawable.ic_baseline_camera_outdoor_24),
             contentDescription = null,
-            modifier = Modifier.size(256.dp)
+            modifier = Modifier.size(256.dp),
+            tint = Color.White
         )
         Text(
             stringResource(R.string.app_name),
-            style = MaterialTheme.typography.h2,
-            textAlign = TextAlign.Center
+            style = MaterialTheme.typography.h2.merge(TextStyle(fontWeight = FontWeight(400))),
+            textAlign = TextAlign.Center,
+            color = Color.White,
         )
-        CircularProgressIndicator()
+        Spacer(Modifier.padding(16.dp))
+        CircularProgressIndicator(
+            color = Color.White,
+            strokeWidth = 8.dp,
+            modifier = Modifier.size(64.dp)
+        )
+    }
+}
+
+@Preview
+@Composable
+fun PreviewSplashScreen() {
+    SplashScreen { deviceType, sensorId ->
+
     }
 }
