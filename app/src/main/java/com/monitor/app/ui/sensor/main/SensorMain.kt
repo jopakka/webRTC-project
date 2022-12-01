fdf85aba336a8863260b32319427bda0a83336fa
Patrik Pölkki <patrik.polkki@elisa.fi>
2022-12-01 13:37:11 +0200

    Design for video view

--- app/src/main/java/com/monitor/app/ui/sensor/main/SensorMain.kt
+++ app/src/main/java/com/monitor/app/ui/sensor/main/SensorMain.kt
@@ -6,12 +6,12 @@ import android.util.Log
 import androidx.activity.compose.BackHandler
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.*
-import androidx.compose.material.FloatingActionButton
-import androidx.compose.material.Icon
-import androidx.compose.material.Scaffold
+import androidx.compose.material.*
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Camera
+import androidx.compose.material.icons.filled.MoreHoriz
 import androidx.compose.runtime.*
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Brush
 import androidx.compose.ui.graphics.Color
@@ -26,6 +26,7 @@ import com.google.accompanist.permissions.ExperimentalPermissionsApi
 import com.google.accompanist.permissions.rememberMultiplePermissionsState
 import com.monitor.app.R
 import com.monitor.app.core.SensorStatuses
+import com.monitor.app.core.components.BatteryLevel
 import com.monitor.app.core.components.KeepScreenOn
 import com.monitor.app.core.components.WebRTCVideoView
 import org.webrtc.SurfaceViewRenderer
@@ -46,6 +47,7 @@ fun SensorMainScreen(
 
     var remoteView by remember { mutableStateOf<SurfaceViewRenderer?>(null) }
     var localView by remember { mutableStateOf<SurfaceViewRenderer?>(null) }
+    var displayMenu by remember { mutableStateOf(false) }
     val permissions = listOf(Manifest.permission.CAMERA, Manifest.permission.RECORD_AUDIO)
     val application = context.applicationContext as Application
     val permissionState = rememberMultiplePermissionsState(permissions = permissions)
@@ -109,7 +111,7 @@ fun SensorMainScreen(
                 Row(
                     modifier = Modifier
                         .fillMaxWidth(1f)
-                        .height(100.dp)
+                        .height(80.dp)
                         .background(
                             brush = Brush.verticalGradient(
                                 0.0f to Color.Black,
@@ -122,8 +124,36 @@ fun SensorMainScreen(
                                 endY = Float.POSITIVE_INFINITY
                             )
                         )
+                        .padding(20.dp),
+                    horizontalArrangement = Arrangement.SpaceBetween,
+                    verticalAlignment = Alignment.CenterVertically
                 ) {
-
+                    BatteryLevel(batteryLevel = 20)
+                    Text(
+                        text = "LIVING ROOM",
+                        style = MaterialTheme.typography.h6,
+                        color = Color.White
+                    )
+                    Box(contentAlignment = Alignment.Center) {
+                        IconButton(onClick = { displayMenu = !displayMenu }) {
+                            Icon(
+                                Icons.Filled.MoreHoriz,
+                                contentDescription = "Options",
+                                tint = Color.White
+                            )
+                        }
+                        DropdownMenu(
+                            expanded = displayMenu,
+                            onDismissRequest = { displayMenu = false }) {
+                            DropdownMenuItem(onClick = {
+                            }) {
+                                Text(text = "Change device type")
+                            }
+                            DropdownMenuItem(onClick = {}) {
+                                Text(text = "Change information")
+                            }
+                        }
+                    }
                 }
             }
         }
